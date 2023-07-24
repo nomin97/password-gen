3862eb7b1143c83ea8efbc2784fa2be320ab0a9e
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-07-24 17:12:58 -0600

    adding changes to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,128 @@
-// Assignment code here
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+// addEventListener to generate password button 
+// series of prompts of password criteria
+// - get length
+// - validate length
+// -include lowercase, uppercase, numeric, special characters
+// -validate character use  
+//  -generate password
+// -show password
+
+// assignment code . generates the generate password button 
+var generateBtn = document.querySelector("#generate")
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// variable definition, criteria for password, arrays for each criteria
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+var passwordCriteria = {
+  passwordLength: 0,
+  alphaLower: alphabet.split(""),
+  alphaUpper: alphabet.toUpperCase().split(""),
+  nums: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  specialChar: ["~", "`", "!", "@", "#", "$", "%", "&", "(", ")", "*", "+", "-", "."],
+}
 
-// Write password to the #password input
+// write password on html 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+//function to generate password
+
+function generatePassword() {
+  var result = "";
+  var passwordLength = 0;
+  var alphabet = "abcdefghijklmnopqrstuvwxyz"
+  var alphaLower = alphabet.split("")
+  var alphaUpper = alphabet.toUpperCase().split("");
+  var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var specialChar = ["~", "`", "!", "@", "#", "$", "%", "&", "(", ")", "*", "+", "-", "."];
+  
+  passwordLength = 0;
+  passwordCriteria.passwordLength = 0;
+  result = "";
+  
+  while (passwordLength < 8 || passwordLength > 128)) {
+    passwordLength = prompt("How Long Should Password Be? (Must be between 8-128 characters.");
+    if (passwordLength === null) {
+      return "Secure Password";
+    } else {
+    }
+    while (passwordCriteria.passwordLength < passwordLength) {
+      var shouldIncludeUppercase = confirm("Click OK to Include a Uppercase Letter in Your Password.");
+    }
+    
+    var shouldIncludeUppercase = confirm("Click OK to Include a Uppercase Letter in Your Password.");
+    var shouldIncludeLowercase = confirm("Click OK to Include a Lowercase Letter in Your Password.");
+    var shouldIncludeNumbers = confirm("Click OK to add number to password.");
+    var shouldIncludeChars = confirm("Click OK to add special characters to password.");
+    
+    // add values to the options if its true
+    if (shouldIncludeUppercase) {
+      options.push(...alphaUpper);
+    }
+    
+    if (shouldIncludeLowercase) {
+      options.push(...alphaLower);
+    }
+    
+    if (shouldIncludeNumbers) {
+      options.push(...nums);
+    }
+    
+    if (shouldIncludeChars) {
+      options.push(...specialChar);
+    }
+    
+    // randomly pick x amount of chars from options and save it to result;
+    var randomLength = function (max, min) {
+      return Math.random() * (max - min) + min;
+    }
+    randomLength(128, 8)
+    
+    var randomUpper = function () {
+      return Math.random(alphaUpper);
+    }
+    randomUpper(alphaUpper)
+    
+    var randomLower = function () {
+      return Math.random(alphaLower);
+    }
+    console.log(randomLower(alphaLower))
+    
+    var randomNumber = function () {
+      return Math.floor(Math.random(nums));
+    }
+    randomNumber(nums)
+    
+    var randomChar = function () {
+      return Math.random(specialChar);
+    }
+    randomChar(specialChar)
+    
+    return generatePassword
+  }
+}
+
+// show password
+
+// event listener, writes password when clicked
 generateBtn.addEventListener("click", writePassword);
